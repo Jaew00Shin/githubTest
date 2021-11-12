fe8edb1c1aca68c7315c8922b3fb9fd204af36ee
Jaewoo <tlsbamtol@snu.ac.kr>
2021-11-12 21:44:04 +0900

    changed

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main(){
-	std::cout<<"Hello, World!"<<std::endl;
+	int sum=0;
+	for(int i=0;i<10;i++){
+		sum+=i;
+	}
+	std::cout<<"summing 1 to 10 is "<<sum<<std::endl;
 	return 0;
+
 }
